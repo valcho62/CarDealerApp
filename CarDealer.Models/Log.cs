a8554410df60bf0e0269ad3d9e71f0d79c95af2f
Valentin Lolov <valio62@yahoo.com>
2017-03-20 17:12:19 +0200

    20-03 rabota

---
+++ CarDealer.Models/Log.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+
+namespace CarDealer.Models
+{
+    public class Log
+    {
+        public int Id { get; set; }
+        public User User { get; set; }
+        public Operation Operation { get; set; }
+        public string ModifiedTable { get; set; }
+        public DateTime DateModified { get; set; }
+
+    }
+    public enum Operation
+    { Add,Edit,Delete }
+}
---
+++ CarDealer.Models/Session.cs
@@ -0,0 +1,15 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace CarDealer.Models
+{
+    public class Session
+    {
+        [Key]
+        public string SessionId { get; set; }
+
+        public bool IsActive { get; set; }
+        public virtual User User { get; set; }
+
+    }
+}
